0a3f708b3fa531f5831c6d7807f772ac7d80593d
micke <miga02@gmail.com>
2021-03-03 20:43:38 +0100

    Added time console app

---
+++ SignalRBlazor.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SignalRBlazor.Console
+{
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+      System.Console.BackgroundColor = ConsoleColor.DarkGray;
+      System.Console.ForegroundColor = ConsoleColor.Green;
+      System.Console.Title = "Time";
+      var connection = new HubConnectionBuilder().WithUrl("https://localhost:44373/timehub").Build();
+
+      await connection.StartAsync();
+
+      connection.On<string>("updateCurrentTime", (time) => {
+        Clear();
+        WriteLine(time);
+      });
+
+      while(connection.State == HubConnectionState.Connected)
+      {
+
+      }
+    }
+  }
+}
